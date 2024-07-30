ff0a728fa831196d2436571a63c3fbfb7a889ba1
MAB-100 <shahabbas661@gmail.com>
2024-07-30 11:01:36 +0500

    defined classes

--- main.py
+++ main.py
@@ -1,8 +1,43 @@
+import pandas
 
-class User:
-    pass
+df_hotels = pandas.read_csv('hotels.csv', dtype={"id": str})
+df_cards = pandas.read_csv('cards.csv' , dtype=str).to_dict(orient='records')
+df_card_security = pandas.read_csv('card_security.csv')
+
 
 class Hostel:
-    pass
+    
+    def __init__(self, hotel_id):
+        pass
 
-class Reser
+    def book(self):
+        pass
+
+    def available(self):
+        pass
+
+
+class ReservationTicket:
+    
+    def __init__():
+        pass
+
+    def generate():
+        pass
+
+
+class CreditCard:
+    def __init__(self, card_number):
+        pass
+
+    def validate(self):
+        pass
+
+
+class CardSecurity(CreditCard):
+    def authenticate(self):
+        pass
+
+
+
+print(df_hotels)
